273e236396292ddff3e618cd899bd4ad4b231338
Dan <texhnistguru@gmail.com>
2024-10-27 20:26:36 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 // COMSC-210 | Lab 25 | Dan Pokhrel
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <chrono>
+#include <vector>
+#include <list>
+#include <set>
 using namespace std;
 
+const int CODES_SZ = 20000;
+
+
+
 int main() {
-    
+    vector<int> vec;
+    list<int> lis;
+    set<int> se;
+
+    // read file
+    ifstream fin("codes.txt");
+    string codes[CODES_SZ];
+    int i = 0;
+    while (fin.good()){
+        string x; getline(fin, x);
+        codes[i] = x;
+        i++;
+    }
+
+
 
     return 0;
 }
